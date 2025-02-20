e7f81b2dc4dcbd7b5fa59f66793ce9b1ac9a5cbb
dileepskb <dileepskb350@gmail.com>
2025-02-20 08:26:50 +0530

    use api integrated

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const config = {
-  //baseURL: process.env.NEXT_APP_BASE_URL,
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.REACT_APP_BASE_URL,
+  //baseURL: "http://localhost:3001" || "https://transport-back-1.onrender.com/",
 };
