0192cd81b42d90bf571f61ba168ee8c105a866d6
dileepskb <dileepskb350@gmail.com>
2025-02-23 05:26:47 +0530

    menu color and dynamic call

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -1,26 +1,13 @@
 import React, { useEffect, useCallback, useRef, useState } from "react";
 import { Link, useLocation } from "react-router";
-import { IconType } from "react-icons";
-import { RxDashboard } from "react-icons/rx";
 
 // Assume these icons are imported from an icon library
 import { useSidebar } from "../context/SidebarContext";
-import {
-  BoxCubeIcon,
-  CalenderIcon,
-  ChevronDownIcon,
-  GridIcon,
-  HorizontaLDots,
-  ListIcon,
-  PageIcon,
-  PieChartIcon,
-  PlugInIcon,
-  TableIcon,
-  UserCircleIcon,
-} from "../icons";
-import SidebarWidget from "./SidebarWidget";
+import { ChevronDownIcon, HorizontaLDots } from "../icons";
 import { Menus, Other } from "../menu/menu";
 import { getItem } from "../utils/storage";
+import { useMenus } from "../hooks/useRoleMenu";
+import DynamicIcon from "../components/dynamicIcon/dynamicIcon";
 
 type NavItem = {
   name: string;
@@ -45,10 +32,8 @@ export const filterMneus = Menus.filter((item, _) =>
 );
 
 const AppSidebar: React.FC = () => {
-  // const menus = useMenus("ADMIN");
-  // console.log(menus);
-
-  const navItems: NavItem[] = filterMneus;
+  const menus = useMenus("ADMIN");
+  const navItems: NavItem[] = menus;
   const othersItems: NavItem[] = Other;
 
   const { isExpanded, isMobileOpen, isHovered, setIsHovered } = useSidebar();
@@ -69,6 +54,8 @@ const AppSidebar: React.FC = () => {
   );
 
   useEffect(() => {
+    if (!navItems.length) return; // Ensure menus are available
+
     let submenuMatched = false;
     ["main", "others"].forEach((menuType) => {
       const items = menuType === "main" ? navItems : othersItems;
@@ -76,10 +63,7 @@ const AppSidebar: React.FC = () => {
         if (nav.subItems) {
           nav.subItems.forEach((subItem) => {
             if (isActive(subItem.path)) {
-              setOpenSubmenu({
-                //type: menuType as "main" | "others",
-                index,
-              });
+              setOpenSubmenu({ index });
               submenuMatched = true;
             }
           });
@@ -90,7 +74,7 @@ const AppSidebar: React.FC = () => {
     if (!submenuMatched) {
       setOpenSubmenu(null);
     }
-  }, [location, isActive]);
+  }, [location.pathname, navItems]); // Ensure it runs when API data updates
 
   useEffect(() => {
     if (openSubmenu !== null) {
@@ -116,137 +100,7 @@ const AppSidebar: React.FC = () => {
       return { index };
     });
   };
-
-  // const renderMenuItems = (items: NavItem[], menuType: "main" | "others") => {
-  //   console.log(items);
-  //   return (
-  //     <ul className="flex flex-col gap-4">
-  //       {items.map((nav, index) => (
-  //         <li key={nav.name}>
-  //           {nav.subItems ? (
-  //             <button
-  //               onClick={() => handleSubmenuToggle(index, menuType)}
-  //               className={`menu-item group ${
-  //                 openSubmenu?.type === menuType && openSubmenu?.index === index
-  //                   ? "menu-item-active"
-  //                   : "menu-item-inactive"
-  //               } cursor-pointer ${
-  //                 !isExpanded && !isHovered
-  //                   ? "lg:justify-center"
-  //                   : "lg:justify-start"
-  //               }`}
-  //             >
-  //               <span
-  //                 className={`${
-  //                   openSubmenu?.type === menuType &&
-  //                   openSubmenu?.index === index
-  //                     ? "menu-item-icon-active"
-  //                     : "menu-item-icon-inactive"
-  //                 }`}
-  //               >
-  //                 {nav.icon as any}
-  //               </span>
-  //               {(isExpanded || isHovered || isMobileOpen) && (
-  //                 <span className="menu-item-text">{nav.name}</span>
-  //               )}
-  //               {(isExpanded || isHovered || isMobileOpen) && (
-  //                 <ChevronDownIcon
-  //                   className={`ml-auto w-5 h-5 transition-transform duration-200 ${
-  //                     openSubmenu?.type === menuType &&
-  //                     openSubmenu?.index === index
-  //                       ? "rotate-180 text-brand-500"
-  //                       : ""
-  //                   }`}
-  //                 />
-  //               )}
-  //             </button>
-  //           ) : (
-  //             nav.path && (
-  //               <Link
-  //                 to={nav.path}
-  //                 className={`menu-item group ${
-  //                   isActive(nav.path)
-  //                     ? "menu-item-active"
-  //                     : "menu-item-inactive"
-  //                 }`}
-  //               >
-  //                 <span
-  //                   className={`${
-  //                     isActive(nav.path)
-  //                       ? "menu-item-icon-active"
-  //                       : "menu-item-icon-inactive"
-  //                   }`}
-  //                 >
-  //                   {nav.icon as any}
-  //                 </span>
-  //                 {(isExpanded || isHovered || isMobileOpen) && (
-  //                   <span className="menu-item-text">{nav.name}</span>
-  //                 )}
-  //               </Link>
-  //             )
-  //           )}
-  //           {nav.subItems && (isExpanded || isHovered || isMobileOpen) && (
-  //             <div
-  //               ref={(el) => {
-  //                 subMenuRefs.current[`${menuType}-${index}`] = el;
-  //               }}
-  //               className="overflow-hidden transition-all duration-300"
-  //               style={{
-  //                 height:
-  //                   openSubmenu?.type === menuType &&
-  //                   openSubmenu?.index === index
-  //                     ? `${subMenuHeight[`${menuType}-${index}`]}px`
-  //                     : "0px",
-  //               }}
-  //             >
-  //               <ul className="mt-2 space-y-1 ml-9">
-  //                 {nav.subItems.map((subItem) => (
-  //                   <li key={subItem.name}>
-  //                     <Link
-  //                       to={subItem.path}
-  //                       className={`menu-dropdown-item ${
-  //                         isActive(subItem.path)
-  //                           ? "menu-dropdown-item-active"
-  //                           : "menu-dropdown-item-inactive"
-  //                       }`}
-  //                     >
-  //                       {subItem.name}
-  //                       <span className="flex items-center gap-1 ml-auto">
-  //                         {subItem.new && (
-  //                           <span
-  //                             className={`ml-auto ${
-  //                               isActive(subItem.path)
-  //                                 ? "menu-dropdown-badge-active"
-  //                                 : "menu-dropdown-badge-inactive"
-  //                             } menu-dropdown-badge`}
-  //                           >
-  //                             new
-  //                           </span>
-  //                         )}
-  //                         {subItem.pro && (
-  //                           <span
-  //                             className={`ml-auto ${
-  //                               isActive(subItem.path)
-  //                                 ? "menu-dropdown-badge-active"
-  //                                 : "menu-dropdown-badge-inactive"
-  //                             } menu-dropdown-badge`}
-  //                           >
-  //                             pro
-  //                           </span>
-  //                         )}
-  //                       </span>
-  //                     </Link>
-  //                   </li>
-  //                 ))}
-  //               </ul>
-  //             </div>
-  //           )}
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   );
-  // };
-
+  console.log(navItems);
   return (
     <aside
       className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-indigo-50 dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
@@ -315,7 +169,7 @@ const AppSidebar: React.FC = () => {
               <ul className="flex flex-col gap-4">
                 {navItems.map((nav, index) => {
                   //const menuType =  "main" | "others"
-                  const { icon: Icon } = nav;
+                  const { icon } = nav;
                   return (
                     <li key={nav.name}>
                       {nav.subItems ? (
@@ -338,7 +192,12 @@ const AppSidebar: React.FC = () => {
                                 : "menu-item-icon-inactive"
                             }`}
                           >
-                            <Icon className="size-5" />
+                            <DynamicIcon
+                              iconName={icon as any}
+                              size={20}
+                              color="red"
+                            />
+                            {/* <Icon className="size-5" /> */}
                           </span>
                           {(isExpanded || isHovered || isMobileOpen) && (
                             <span className="menu-item-text">{nav.name}</span>
@@ -371,7 +230,11 @@ const AppSidebar: React.FC = () => {
                                   : "menu-item-icon-inactive"
                               }`}
                             >
-                              <Icon className="size-5" />
+                              <DynamicIcon
+                                iconName={icon as any}
+                                size={20}
+                                // color="indigo"
+                              />
                             </span>
                             {(isExpanded || isHovered || isMobileOpen) && (
                               <span className="menu-item-text">{nav.name}</span>
